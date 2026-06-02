61a5ff5163ff55c9db200aa2183e77d2dcb69a2c
Yuliia <ciceron131313@gmail.com>
2026-06-02 19:37:17 +0300

    Update notesValidation.js

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -17,7 +17,9 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string(),
+    tag: Joi.string()
+             .valid(...tags)
+             .optional,
     search: Joi.string().trim().allow(""),
   }),
 };
@@ -31,7 +33,9 @@ export const updateNoteChema = {
  [Segments.BODY]: Joi.object({
   title: Joi.string().min(1),
   content: Joi.string(""),
-  tag: Joi.string(),
+  tag: Joi.string()
+          .valid(...tags)
+          .optional,
  }).min(1),
 };
 
@@ -39,6 +43,8 @@ export const createNoteSchema = {
 [ Segments.BODY]: Joi.object({
   title: Joi.string().min(1).required(),
   content: Joi.string(""),
-  tag: Joi.string(),
+  tag: Joi.string()
+          .valid(...tags)
+          .optional,
 }),
 };
