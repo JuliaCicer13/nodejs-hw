fb8f921554c2b8a39f427a5b88ba58fdcea1cc29
Yuliia <ciceron131313@gmail.com>
2026-06-02 20:39:39 +0300

    Update notesValidation.js

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -19,7 +19,7 @@ export const getAllNotesSchema = {
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string()
-             .valid(...TAGS)
+             .valid(...tags)
              .optional(),
     search: Joi.string().trim().allow(""),
   }),
