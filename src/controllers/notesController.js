f6cbb5251ce174646a3220c48b153c2555fa000d
Yuliia <ciceron131313@gmail.com>
2026-06-03 17:38:55 +0300

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -13,21 +13,14 @@ export const getAllNotes = async (req,res) => {
 
   const notesQuery = Note.find();
 
-  if(search) {
-     notesQuery.where("search").equals(search);
-  }
-
-  if(tag) {
-    notesQuery.where("tag").equals(tag);
-  }
-     if(search) {
+  
     notesQuery.where({
       $or: [
-        {title: { $regex: search, $options: 'i' } },
-        {content: {$regex: search, $options: 'i'} },
+        {title: { $regex: search, $options: 'search' } },
+        {content: {$regex: search, $options: 'tag'} },
       ],
     });
-     }
+
 
     const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
