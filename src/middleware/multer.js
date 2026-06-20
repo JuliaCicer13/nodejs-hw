b4b5650c09036ca76af6246ff51be88aea0f9245
Yuliia <ciceron131313@gmail.com>
2026-06-20 20:58:13 +0300

    k

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -6,7 +6,7 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-	  if (file.mimetype.startsWith('/image')) {
+	  if (file.mimetype.startsWith('image/')) {
 	    cb(null, true)
 	  } else {
 	    cb(new Error('Invalid file type. Only JPEG, PNG, GIF, and WebP are allowed.'), false)
