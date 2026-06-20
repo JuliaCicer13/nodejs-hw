23d67c5fdd3eda8a058db102bc466f2a0211cebe
Yuliia <ciceron131313@gmail.com>
2026-06-20 20:54:46 +0300

    kkkkkkkk

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -10,6 +10,6 @@ const transporter = nodemailer.createTransport({
 });
 
 
-export const sendMail = async (options) => {
-  return await transporter.sendMail(options);
+export const sendEmail = async (options) => {
+  return await transporter.sendEmail(options);
 };
